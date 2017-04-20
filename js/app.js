6b2b61339cf51523abcd7c6b22e3fa4b3cd7b0d6
Tim Hall <timhall3000@gmail.com>
2017-04-19 23:51:43 -0700

    Implement MVVM pattern

--- js/app.js
+++ js/app.js
@@ -1,11 +1,38 @@
+// Initialize map and infoWindow
 var map, infoWindow;
-var locations = {
-    "Cassia": "1314 7th St, Santa Monica, CA 90401",
-    "Mastro's Steakhouse": "246 N Canon Dr, Beverly Hills, CA 90210",
-    "Angelini Osteria": "7313 Beverly Blvd, Los Angeles, CA 90036",
-    "Kotoya": "10422 National Blvd, Los Angeles, CA 90034",
-    "Katsu-ya": "11680 Ventura Blvd, Studio City, CA 91604"
-}
+
+
+// MODEL
+var model = {
+    restaurants: [
+        {
+            name: "Cassia",
+            address: '1314 7th St, Santa Monica, CA 90401'
+        },
+        {
+            name: "Mastro's Steakhouse",
+            address: '246 N Canon Dr, Beverly Hills, CA 90210'
+        },
+        {
+            name: "Angelini Osteria",
+            address: '7313 Beverly Blvd, Los Angeles, CA 90036'
+        },
+        {
+            name: "Kotoya",
+            address: '10422 National Blvd, Los Angeles, CA 90034'
+        },
+        {
+            name: "Katsu-ya",
+            address: '11680 Ventura Blvd, Studio City, CA 91604'
+        }
+
+        // "Cassia": "1314 7th St, Santa Monica, CA 90401",
+        // "Mastro's Steakhouse": "246 N Canon Dr, Beverly Hills, CA 90210",
+        // "Angelini Osteria": "7313 Beverly Blvd, Los Angeles, CA 90036",
+        // "Kotoya": "10422 National Blvd, Los Angeles, CA 90034",
+        // "Katsu-ya": "11680 Ventura Blvd, Studio City, CA 91604"
+    ]
+};
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -44,8 +71,14 @@ function initMap() {
     }
 
 
-    Object.keys(locations).forEach(function(key) {
-        console.log(key, locations[key]);
-        geocodeAddress(geocoder, map, locations[key]);
-    });
+    // Object.keys(locations).forEach(function(key) {
+    //     console.log(key, locations[key]);
+    //     geocodeAddress(geocoder, map, locations[key]);
+    // });
+
+    for (var i = 0; i < model.restaurants.length; i++) {
+        console.log(model.restaurants[i].name);
+        console.log(model.restaurants[i].address);
+        geocodeAddress(geocoder, map, model.restaurants[i].address);
+    }
 }
